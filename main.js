4311c2cd143b8ef69d58676769dfae9fb849db69
Nikos Kapraras <nikos@kapraran.dev>
2021-12-24 02:34:02 +0100

    add yargs

--- main.js
+++ main.js
@@ -3,22 +3,27 @@ const { readHavokLuaFile } = require("./scripts/read-havok-lua");
 const { readMapEbx } = require("./scripts/read-map-ebx");
 const { createSave } = require("./scripts/create-save");
 const saveFilesData = require("./config/saveFiles");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
 
 async function main() {
   // const havokTransforms = await readHavokLuaFile(
   //   resolve(__dirname, "assets/havok.lua")
   // );
 
-  havokTransforms = require(resolve(__dirname, "./assets/data.json"))
+  havokTransforms = require(resolve(__dirname, "./assets/data.json"));
 
-  let saveFilesDataToUse = saveFilesData
+  let saveFilesDataToUse = saveFilesData;
+
+  if (argv.tryFixMissingAssets) console.log('yes i will')
 
   // if you want to create a save for a single project
   // uncomment the next line an put the correct project name
   // saveFilesDataToUse = [saveFilesData.find(d => d.projectName === 'XP5_003 Havok MapEd Save')]
 
   for (const saveFileConfigData of saveFilesDataToUse) {
-    console.log(saveFileConfigData.projectName)
+    console.log(saveFileConfigData.projectName);
 
     const items = await readMapEbx(havokTransforms, saveFileConfigData);
     const saveFilePath = resolve(
